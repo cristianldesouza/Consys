ba0d03edd56e82a065cf90ecf25fc95e05bc2bcd
keony1 <keony@edu.univali.br>
2019-11-21 09:53:36 -0300

    feat: verificação se o cliente/motorista existe em algum registro não deixa excluir

--- API/src/controllers/ClientesController.js
+++ API/src/controllers/ClientesController.js
@@ -1,46 +1,50 @@
-import Clientes from '../models/Clientes';
+import Clientes from "../models/Clientes";
 
 module.exports = {
-    async index(req, res) {
-        const listaDeClientes = await Clientes.findAll();
-        
-        return res.status(200).json(listaDeClientes);
-    },
+  async index(req, res) {
+    const listaDeClientes = await Clientes.findAll();
 
-    async store(req, res) {
+    return res.status(200).json(listaDeClientes);
+  },
 
-        let cliente = {
-            nome_fantasia: req.body.nomeFantasia,
-            cnpj: req.body.cnpj,
-            email: req.body.email,
-            telefone: req.body.telefone
-        }
+  async store(req, res) {
+    let cliente = {
+      nome_fantasia: req.body.nomeFantasia,
+      cnpj: req.body.cnpj,
+      email: req.body.email,
+      telefone: req.body.telefone
+    };
 
-        await Clientes.create(cliente)
+    await Clientes.create(cliente);
 
-        cliente = await Clientes.findAll();
+    cliente = await Clientes.findAll();
 
-        cliente = cliente[cliente.length - 1];
+    cliente = cliente[cliente.length - 1];
 
-        return res.status(200).json(cliente)
+    return res.status(200).json(cliente);
+  },
 
-    },
+  async delete(req, res) {
+    const clienteId = req.params.id;
 
-    async delete(req, res) {
+    const jaExiste = await Clientes.findOne({ where: { id: clienteId } });
 
-        const clienteId = req.params.id;
-        
-        const jaExiste = await Clientes.findOne({ where: { id: clienteId } });
-
-        if (!jaExiste) {
-            return res.status(400).json({ status: "Id inexistente." })
-        }
-
-
-        const cliente = await Clientes.findOne({ where: { id: clienteId } });
-
-        await Clientes.destroy({ where: { id: clienteId } });
-
-        return res.status(200).json(cliente);
+    if (!jaExiste) {
+      return res.status(400).json({ status: "Id inexistente." });
     }
-}
+
+    const cliente = await Clientes.findOne({ where: { id: clienteId } });
+
+    try {
+      await Clientes.destroy({ where: { id: clienteId } });
+    } catch (err) {
+      return res
+        .status(400)
+        .json({
+          error: "Não é possível deletar clientes vinculados a registros"
+        });
+    }
+
+    return res.status(200).json(cliente);
+  }
+};
--- API/src/controllers/MotoristasController.js
+++ API/src/controllers/MotoristasController.js
@@ -1,43 +1,48 @@
-import Motoristas from '../models/Motoristas';
+import Motoristas from "../models/Motoristas";
 
 module.exports = {
-    async index(req, res) {
-        const listaDeMotoristas = await Motoristas.findAll();
-        
-        return res.status(200).json(listaDeMotoristas);
-    },
+  async index(req, res) {
+    const listaDeMotoristas = await Motoristas.findAll();
 
-    async store(req, res) {
+    return res.status(200).json(listaDeMotoristas);
+  },
 
-        let motorista = {
-            nome: req.body.nome,
-            cpf: req.body.cpf,
-            telefone: req.body.telefone
-        }
+  async store(req, res) {
+    let motorista = {
+      nome: req.body.nome,
+      cpf: req.body.cpf,
+      telefone: req.body.telefone
+    };
 
-        await Motoristas.create(motorista)
+    await Motoristas.create(motorista);
 
-        motorista = await Motoristas.findAll();
+    motorista = await Motoristas.findAll();
 
-        motorista = motorista[motorista.length -1];
+    motorista = motorista[motorista.length - 1];
 
-        return res.status(200).json(motorista)
+    return res.status(200).json(motorista);
+  },
 
-    },
+  async delete(req, res) {
+    const motoristaId = req.params.id;
 
-    async delete(req, res) {
-        const motoristaId = req.params.id;
+    const jaExiste = await Motoristas.findOne({ where: { id: motoristaId } });
 
-        const jaExiste = await Motoristas.findOne({ where: { id: motoristaId } });
-
-        if (!jaExiste) {
-            return res.status(400).json({ status: "Id inexistente." })
-        }
-
-        const motorista = await Motoristas.findOne({ where: { id: motoristaId } });
-
-        await Motoristas.destroy({ where: { id: motoristaId } });
-
-        return res.status(200).json(motorista);
+    if (!jaExiste) {
+      return res.status(400).json({ status: "Id inexistente." });
     }
-}
+
+    const motorista = await Motoristas.findOne({ where: { id: motoristaId } });
+    try {
+      await Motoristas.destroy({ where: { id: motoristaId } });
+    } catch (err) {
+      return res
+        .status(400)
+        .json({
+          error: "Não é possível deletar Motoristas vinculados a registros"
+        });
+    }
+
+    return res.status(200).json(motorista);
+  }
+};
